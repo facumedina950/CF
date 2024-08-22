4d44bcacff3ca7ff7954ff932b1d0628a2b80498
facumedina950 <facumedina950@gmail.com>
2024-08-22 13:10:44 -0300

    Segundo commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+
+
+constructor(private formulario:FormBuilder){}
+
+usuarios:FormGroup = this.formulario.group({
+  email:['Luciando@email.com', [Validators.required, Validators.email]],
+  password:['contraseña',[Validators.required, Validators.minLength(8),]],
+});
+
+ngOnInit(): void {
+  
+}
+
+muestreUsuario(){
+  console.log(this.usuarios.value)
+}
+
+mostrarUsuario:string = this.usuarios.controls['email'].value
 
 }
